846148c8ebd70bdd6c8f54220a1e7ffc05fc0b7e
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-05-28 22:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+// Focus first input on load
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    // allow only single digit
+    e.target.value = value.replace(/[^0-9]/g, '').slice(0, 1);
+
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+
+    if (e.key === 'Backspace') {
+
+      if (input.value === '' && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = '';
+        e.preventDefault();
+      }
+    }
+  });
+
+});
